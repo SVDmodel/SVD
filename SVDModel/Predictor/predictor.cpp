132821c06addccb4e2199876947216da8843132d
Werner Rammer <werner.rammer@boku.ac.at>
2017-06-26 15:58:37 +0200

    initial projects

---
+++ SVDModel/Predictor/predictor.cpp
@@ -0,0 +1,6 @@
+#include "predictor.h"
+
+
+Predictor::Predictor()
+{
+}
---
+++ SVDModel/Predictor/predictor.h
@@ -0,0 +1,12 @@
+#ifndef PREDICTOR_H
+#define PREDICTOR_H
+
+
+class Predictor
+{
+
+public:
+    Predictor();
+};
+
+#endif // PREDICTOR_H
